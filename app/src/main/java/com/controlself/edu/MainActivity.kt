6fb2afc84c43bc7b4f09576717cd6f7ed8b868c0
Cesar Eduardo Lizama Camero <U20201545@utp.edu.pe>
2026-07-20 01:32:46 -0500

    padding superior e inferior universal

--- app/src/main/java/com/controlself/edu/MainActivity.kt
+++ app/src/main/java/com/controlself/edu/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
@@ -36,7 +37,9 @@ class MainActivity : ComponentActivity() {
             CompositionLocalProvider(LocalAppContainer provides app.container) {
                 ControlSelfEDUTheme {
                     Surface(
-                        modifier = Modifier.fillMaxSize(),
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .safeDrawingPadding(),
                         color = CseWhite
                     ) {
                         ControlSelfNavHost()
